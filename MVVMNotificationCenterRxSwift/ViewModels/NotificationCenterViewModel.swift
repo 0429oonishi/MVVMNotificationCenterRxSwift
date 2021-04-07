44c9e973bf8376ea8b97927b77bcf2b3800b73c5
0429oonishi <oonishireon110@gmail.com>
2021-04-07 19:36:34 +0900

    コメントを追加

--- MVVMNotificationCenterRxSwift/ViewModels/NotificationCenterViewModel.swift
+++ MVVMNotificationCenterRxSwift/ViewModels/NotificationCenterViewModel.swift
@@ -19,14 +19,19 @@ final class NotificationCenterViewModel {
         self.model = model
     }
     
+    //Viewからイベントを受け取り、Modelの処理を呼び出す
     func idPasswordChanged(id: String?, password: String?) {
         let result = model.validate(idText: id, passwordText: password)
         switch result {
         case .success:
+            //Viewからイベントを受け取り、加工して値を更新する
             notificationCenter.post(name: changeText, object: "OK!!!")
+            //Viewからイベントを受け取り、加工して値を更新する
             notificationCenter.post(name: changeColor, object: UIColor.green)
         case .failure(let error as ModelError):
+            //Viewからイベントを受け取り、加工して値を更新する
             notificationCenter.post(name: changeText, object: error.errorText)
+            //Viewからイベントを受け取り、加工して値を更新するº
             notificationCenter.post(name: changeColor, object: UIColor.red)
         case _:
             fatalError("Unexpected pattern.")
--- MVVMNotificationCenterRxSwift/Views/ViewControllers/NotificationCenterViewController.swift
+++ MVVMNotificationCenterRxSwift/Views/ViewControllers/NotificationCenterViewController.swift
@@ -21,7 +21,9 @@ final class NotificationCenterViewController: UIViewController {
         
         idTextField.addTarget(self, action: #selector(textFieldEditignChanged), for: .editingChanged)
         passwordTextField.addTarget(self, action: #selector(textFieldEditignChanged), for: .editingChanged)
+        //自身の状態とViewModelの状態をデーターバインディングする
         notificationCenter.addObserver(self, selector: #selector(updateValidationText), name: viewModel.changeText, object: nil)
+        //自身の状態とViewModelの状態をデーターバインディングする
         notificationCenter.addObserver(self, selector: #selector(updateValidationColor), name: viewModel.changeColor, object: nil)
         
     }
@@ -31,15 +33,18 @@ final class NotificationCenterViewController: UIViewController {
 private extension NotificationCenterViewController {
     
     @objc func textFieldEditignChanged(sender: UITextField) {
+        //ユーザーの入力をViewModelに渡す
         viewModel.idPasswordChanged(id: idTextField.text, password: passwordTextField.text)
     }
     
     @objc func updateValidationText(notification: Notification) {
+        //ViewModelから返されるイベントを元に描画処理を実行する
         guard let text = notification.object as? String else { return }
         validationLabel.text = text
     }
     
     @objc func updateValidationColor(notification: Notification) {
+        //ViewModelから返されるイベントを元に描画処理を実行する
         guard let color = notification.object as? UIColor else { return }
         validationLabel.textColor = color
     }
